84296470b76ddcadc6935bb4f8ccd016cdf29fbd
Zbigniew Hadrian <zbigniew.hadrian@unity.pl>
2021-10-19 10:08:11 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <Tutorial/>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "@vue/composition-api";
+import Kitsu from "kitsu";
+
+export default defineComponent({
+  setup() {
+    const kitsu = new Kitsu()
+    // In SSR throws [object Object] is not an Option Object
+    kitsu.fetch('/abstract-products', {
+      params: {
+        include: 'abstract-product-image-sets,abstract-product-prices',
+        fields: {
+          'abstract-products': 'name,age',
+          'abstract-product-image-sets': 'id,imageSets',
+          'abstract-product-prices': 'price',
+        },
+      },
+    }).then(
+        (response: any) => {
+            console.log('abstract product response ', response)
+        }
+    )
+  }
+})
+</script>
